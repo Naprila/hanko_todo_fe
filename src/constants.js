dadbb0d60a4fe11f3468d1d5d15a49d870080d35
Apple <apple@Apples-MacBook-Pro.local>
2024-10-16 01:16:12 +0530

    updated the backend url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
 export const hankoApi = process.env.REACT_APP_HANKO_API_URL;
 
-export const BACKEND_URL = "http://localhost:3001"
+const isProduction = process.env.NODE_ENV === "production";
+export const BACKEND_URL = isProduction
+  ? "https://hanko-todo-be.onrender.com"
+  : "http://localhost:3001";
+
